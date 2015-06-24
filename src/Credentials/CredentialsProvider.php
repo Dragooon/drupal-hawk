265c18016af98d1a84f05450bc42831e54fc275d
Dragooon <mail@dragooon.net>
2015-06-24 16:14:53 +0530

    Implement CredentialsProvider using EntityManager

--- src/Credentials/CredentialsProvider.php
+++ src/Credentials/CredentialsProvider.php
@@ -2,15 +2,35 @@
 
 namespace Drupal\Hawk\Credentials;
 
+use Dragooon\Hawk\Credentials\Credentials;
 use Dragooon\Hawk\Credentials\CredentialsProviderInterface;
+use Drupal\Core\Entity\EntityManagerInterface;
 
 class CredentialsProvider implements CredentialsProviderInterface {
-  
+
+  /**
+   * @var EntityManagerInterface
+   */
+  protected $entityManager;
+
+  /**
+   * @param EntityManagerInterface $entityManager
+   */
+  public function __construct(EntityManagerInterface $entityManager) {
+    $this->entityManager = $entityManager;
+  }
+
   /**
    * {@inheritDoc}
    */
   public function loadCredentialsById($id) {
+    $credential = $this->entityManager->getStorage('hawk_credential')->load($id);
 
+    if (empty($credential)) {
+      return false;
+    }
+
+    return new Credentials($credential->getKeySecret(), $credential->getKeyAlgo(), $credential->id());
   }
 
 }
