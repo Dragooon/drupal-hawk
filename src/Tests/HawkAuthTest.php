18c529dd9d614ce953e3ede73084de0d81116738
Shitiz Garg <mail@dragooon.net>
2015-08-14 02:07:54 +0530

    Add unit test for testing replay attack protection

--- src/Tests/HawkAuthTest.php
+++ src/Tests/HawkAuthTest.php
@@ -7,8 +7,11 @@
 
 namespace Drupal\hawk_auth\Tests;
 
+use Drupal\Core\Session\AccountInterface;
+use Drupal\hawk_auth\Entity\HawkCredentialInterface;
 use Drupal\simpletest\WebTestBase;
 use Drupal\Core\Url;
+use Drupal\user\UserInterface;
 
 /**
  * Tests for basic hawk authentication.
@@ -27,27 +30,64 @@ class HawkAuthTest extends WebTestBase {
   public static $modules = ['hawk_auth', 'hawk_route_tests'];
 
   /**
-   * Test hawk auth authentication.
+   * Account we'll be using for testing Hawk.
+   *
+   * @var UserInterface
    */
-  public function testHawkAuth() {
-    $account = $this->drupalCreateUser();
-    $credential = $this->getHawkCredentials($account);
+  protected $account;
 
+  /**
+   * Credentials for the above account we'll be using for testing Hawk.
+   *
+   * @var HawkCredentialInterface
+   */
+  protected $credential;
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp() {
+    parent::setUp();
+
+    $this->account = $this->drupalCreateUser();
+    $this->credential = $this->getHawkCredentials($this->account);
+  }
+
+  /**
+   * Test basic hawk auth authentication response.
+   */
+  public function testHawkAuthResponse() {
     $url = Url::fromRoute('hawk_route_test.user');
 
-    $this->hawkAuthGet($url, $credential);
-    $this->assertText($account->getUsername(), 'Account name is displayed');
+    $this->hawkAuthGet($url, $this->credential);
+    $this->assertText($this->account->getUsername(), 'Account name is displayed');
     $this->assertResponse('200', 'HTTP Response is okay');
     $this->curlClose();
     $this->assertFalse($this->drupalGetHeader('X-Drupal-Cache'));
     $this->assertIdentical(strpos($this->drupalGetHeader('Cache-Control'), 'public'), FALSE, 'Cache-Control is not set to public');
 
-    $wrong_credential = clone $credential;
+    $wrong_credential = clone $this->credential;
     $wrong_credential->setKeySecret('wrong_key');
 
     $this->hawkAuthGet($url, $wrong_credential);
-    $this->assertNoText($account->getUsername(), 'Account name should not be displayed.');
+    $this->assertNoText($this->account->getUsername(), 'Account name should not be displayed.');
     $this->assertResponse('403', 'HTTP Access is not granted');
     $this->curlClose();
+  }
+
+  /**
+   * Tests for protection against replay attacks.
+   */
+  public function testHawkReplayAttack() {
+    $url = Url::fromRoute('hawk_route_test.user');
+
+    $header = $this->getHawkAuthHeader($url, $this->credential);
+    $this->hawkAuthGet($url, $this->credential, ['header' => $header]);
+    $this->assertResponse('200', 'HTTP Response is okay');
+    $this->assertText($this->account->getUsername(), 'Account name is displayed');
+
+    $this->hawkAuthGet($url, $this->credential, ['header' => $header]);
+    $this->assertResponse('403', 'HTTP Response is okay for nonce validation failure');
+    $this->assertNoText($this->account->getUsername(), 'Account name should not be displayed.');
   }
 }
